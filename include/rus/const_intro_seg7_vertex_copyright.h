64f0d575f93f40abaa57cf378ca24e55a9ab83d2
AlexN <102762032+Al-ex-N@users.noreply.github.com>
2024-09-13 23:45:46 +0300

    Обновление 7.2

--- include/rus/const_intro_seg7_vertex_copyright.h
+++ include/rus/const_intro_seg7_vertex_copyright.h
@@ -2,17 +2,17 @@
 #define RUS_CONST_INTRO_SEG7_VERTEX_COPYRIGHT_H
 
 static const Vtx intro_seg7_vertex_rus_version[] = {
-    {{{    47,     26,     -1}, 0, {     0,    512}, {0xff, 0xff, 0xff, 0xff}}},
-    {{{   175,     26,     -1}, 0, {  4096,    512}, {0xff, 0xff, 0xff, 0xff}}},
-    {{{   175,     42,     -1}, 0, {  4096,      0}, {0xff, 0xff, 0xff, 0xff}}},
-    {{{    47,     42,     -1}, 0, {     0,      0}, {0xff, 0xff, 0xff, 0xff}}},
+    {{{    43,     26,     -1}, 0, {     0,    512}, {0xff, 0xff, 0xff, 0xff}}},
+    {{{   171,     26,     -1}, 0, {  4096,    512}, {0xff, 0xff, 0xff, 0xff}}},
+    {{{   171,     42,     -1}, 0, {  4096,      0}, {0xff, 0xff, 0xff, 0xff}}},
+    {{{    43,     42,     -1}, 0, {     0,      0}, {0xff, 0xff, 0xff, 0xff}}},
 };
 
 static const Vtx intro_seg7_vertex_rus_copyright[] = {
-    {{{   157,     26,     -1}, 0, {     0,    512}, {0xff, 0xff, 0xff, 0xff}}},
-    {{{   285,     26,     -1}, 0, {  4096,    512}, {0xff, 0xff, 0xff, 0xff}}},
-    {{{   285,     42,     -1}, 0, {  4096,      0}, {0xff, 0xff, 0xff, 0xff}}},
-    {{{   157,     42,     -1}, 0, {     0,      0}, {0xff, 0xff, 0xff, 0xff}}},
+    {{{   171,     26,     -1}, 0, {     0,    512}, {0xff, 0xff, 0xff, 0xff}}},
+    {{{   299,     26,     -1}, 0, {  4096,    512}, {0xff, 0xff, 0xff, 0xff}}},
+    {{{   299,     42,     -1}, 0, {  4096,      0}, {0xff, 0xff, 0xff, 0xff}}},
+    {{{   171,     42,     -1}, 0, {     0,      0}, {0xff, 0xff, 0xff, 0xff}}},
 };
 
 #endif // This will be used in "levels/intro/leveldata.c"
