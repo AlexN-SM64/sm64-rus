0ca9997f83466866099ecb85ec760183356f6c40
AlexN <102762032+Al-ex-N@users.noreply.github.com>
2024-09-13 23:34:43 +0300

    Обновление 7.1

--- include/rus/const_intro_seg7_texture_copyright.h
+++ include/rus/const_intro_seg7_texture_copyright.h
@@ -1,12 +1,12 @@
 #ifndef RUS_CONST_INTRO_SEG7_TEXTURE_COPYRIGHT_H
 #define RUS_CONST_INTRO_SEG7_TEXTURE_COPYRIGHT_H
 
-ALIGNED8 static const Texture intro_seg7_texture_copyright_rus_1[] = {
-#include "levels/intro/texture_copyright_rus_1.rgba16.inc.c"
+ALIGNED8 static const Texture intro_seg7_texture_rus_version[] = {
+#include "levels/intro/texture_rus_version.rgba16.inc.c"
 };
 
-ALIGNED8 static const Texture intro_seg7_texture_copyright_rus_2[] = {
-#include "levels/intro/texture_copyright_rus_2.rgba16.inc.c"
+ALIGNED8 static const Texture intro_seg7_texture_rus_copyright[] = {
+#include "levels/intro/texture_rus_copyright.rgba16.inc.c"
 };
 
 #endif // This will be used in "levels/intro/leveldata.c"
--- include/rus/const_intro_seg7_vertex_copyright.h
+++ include/rus/const_intro_seg7_vertex_copyright.h
@@ -1,18 +1,18 @@
 #ifndef RUS_CONST_INTRO_SEG7_VERTEX_COPYRIGHT_H
 #define RUS_CONST_INTRO_SEG7_VERTEX_COPYRIGHT_H
 
-static const Vtx intro_seg7_vertex_copyright_rus_1[] = {
-    {{{    32,     26,     -1}, 0, {     0,    512}, {0xff, 0xff, 0xff, 0xff}}},
-    {{{   160,     26,     -1}, 0, {  4096,    512}, {0xff, 0xff, 0xff, 0xff}}},
-    {{{   160,     42,     -1}, 0, {  4096,      0}, {0xff, 0xff, 0xff, 0xff}}},
-    {{{    32,     42,     -1}, 0, {     0,      0}, {0xff, 0xff, 0xff, 0xff}}},
+static const Vtx intro_seg7_vertex_rus_version[] = {
+    {{{    47,     26,     -1}, 0, {     0,    512}, {0xff, 0xff, 0xff, 0xff}}},
+    {{{   175,     26,     -1}, 0, {  4096,    512}, {0xff, 0xff, 0xff, 0xff}}},
+    {{{   175,     42,     -1}, 0, {  4096,      0}, {0xff, 0xff, 0xff, 0xff}}},
+    {{{    47,     42,     -1}, 0, {     0,      0}, {0xff, 0xff, 0xff, 0xff}}},
 };
 
-static const Vtx intro_seg7_vertex_copyright_rus_2[] = {
-    {{{   160,     26,     -1}, 0, {     0,    512}, {0xff, 0xff, 0xff, 0xff}}},
-    {{{   288,     26,     -1}, 0, {  4096,    512}, {0xff, 0xff, 0xff, 0xff}}},
-    {{{   288,     42,     -1}, 0, {  4096,      0}, {0xff, 0xff, 0xff, 0xff}}},
-    {{{   160,     42,     -1}, 0, {     0,      0}, {0xff, 0xff, 0xff, 0xff}}},
+static const Vtx intro_seg7_vertex_rus_copyright[] = {
+    {{{   157,     26,     -1}, 0, {     0,    512}, {0xff, 0xff, 0xff, 0xff}}},
+    {{{   285,     26,     -1}, 0, {  4096,    512}, {0xff, 0xff, 0xff, 0xff}}},
+    {{{   285,     42,     -1}, 0, {  4096,      0}, {0xff, 0xff, 0xff, 0xff}}},
+    {{{   157,     42,     -1}, 0, {     0,      0}, {0xff, 0xff, 0xff, 0xff}}},
 };
 
 #endif // This will be used in "levels/intro/leveldata.c"
