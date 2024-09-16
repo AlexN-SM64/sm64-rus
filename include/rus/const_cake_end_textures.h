783b102b85347118a25a218bb4b04ea5fc63d7c2
AlexN <102762032+Al-ex-N@users.noreply.github.com>
2024-09-16 21:17:30 +0300

    Обновление 10, убрать оригины с текстуры торта

---
+++ include/rus/const_cake_end_textures.h
@@ -0,0 +1,199 @@
+#ifndef RUS_CONST_CAKE_END_TEXTURES_H
+#define RUS_CONST_CAKE_END_TEXTURES_H
+
+// All tiles of the original Cake texture were split and written in PNG using tools/skyconv
+// and imported constants with includes from "cake.inc.c".
+
+ALIGNED8 static const Texture cake_end_texture_0[] = {
+#include "levels/ending/cake.0.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_1[] = {
+#include "levels/ending/cake.1.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_2[] = {
+#include "levels/ending/cake.2.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_3[] = {
+#include "levels/ending/cake.3.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_4[] = {
+#include "levels/ending/cake.4.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_5[] = {
+#include "levels/ending/cake.5.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_6[] = {
+#include "levels/ending/cake.6.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_7[] = {
+#include "levels/ending/cake.7.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_8[] = {
+#include "levels/ending/cake.8.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_9[] = {
+#include "levels/ending/cake.9.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_10[] = {
+#include "levels/ending/cake.10.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_11[] = {
+#include "levels/ending/cake.11.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_12[] = {
+#include "levels/ending/cake.12.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_13[] = {
+#include "levels/ending/cake.13.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_14[] = {
+#include "levels/ending/cake.14.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_15[] = {
+#include "levels/ending/cake.15.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_16[] = {
+#include "levels/ending/cake.16.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_17[] = {
+#include "levels/ending/cake.17.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_18[] = {
+#include "levels/ending/cake.18.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_19[] = {
+#include "levels/ending/cake.19.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_20[] = {
+#include "levels/ending/cake.20.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_21[] = {
+#include "levels/ending/cake.21.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_22[] = {
+#include "levels/ending/cake.22.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_23[] = {
+#include "levels/ending/cake.23.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_24[] = {
+#include "levels/ending/cake.24.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_25[] = {
+#include "levels/ending/cake_rus.25.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_26[] = {
+#include "levels/ending/cake_rus.26.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_27[] = {
+#include "levels/ending/cake.27.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_28[] = {
+#include "levels/ending/cake.28.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_29[] = {
+#include "levels/ending/cake_rus.29.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_30[] = {
+#include "levels/ending/cake_rus.30.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_31[] = {
+#include "levels/ending/cake.31.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_32[] = {
+#include "levels/ending/cake.32.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_33[] = {
+#include "levels/ending/cake.33.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_34[] = {
+#include "levels/ending/cake.34.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_35[] = {
+#include "levels/ending/cake.35.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_36[] = {
+#include "levels/ending/cake.36.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_37[] = {
+#include "levels/ending/cake.37.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_38[] = {
+#include "levels/ending/cake.38.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_39[] = {
+#include "levels/ending/cake.39.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_40[] = {
+#include "levels/ending/cake.40.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_41[] = {
+#include "levels/ending/cake_rus.41.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_42[] = {
+#include "levels/ending/cake_rus.42.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_43[] = {
+#include "levels/ending/cake.43.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_44[] = {
+#include "levels/ending/cake.44.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_45[] = {
+#include "levels/ending/cake_rus.45.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_46[] = {
+#include "levels/ending/cake_rus.46.rgba16.inc.c"
+};
+
+ALIGNED8 static const Texture cake_end_texture_47[] = {
+#include "levels/ending/cake.47.rgba16.inc.c"
+};
+
+#endif // This will be used in "levels/ending/leveldata.c"
