fafd64637274212928a58c0c2567aee43e765809
AlexN <102762032+Al-ex-N@users.noreply.github.com>
2024-11-09 20:04:34 +0300

    Коррекция позиции текста "ВРЕМЯ"

--- src/game/hud.c
+++ src/game/hud.c
@@ -355,7 +355,7 @@ void render_hud_timer(void) {
 
     timerFracSecs = ((timerValFrames - (timerMins * 1800) - (timerSecs * 30)) & 0xFFFF) / 3;
 #ifndef VERSION_EU
-    print_text(GFX_DIMENSIONS_RECT_FROM_RIGHT_EDGE(TXT_TIME_X_OUTPUT), 185, TXT_HUD_TIME); //! ONLY IN RUSSIAN VERSION
+    print_text(TXT_TIME_X_OUTPUT, 185, TXT_HUD_TIME); //! ONLY IN RUSSIAN VERSION
 #endif
     print_text_fmt_int(GFX_DIMENSIONS_RECT_FROM_RIGHT_EDGE(91), 185, "%0d", timerMins);
     print_text_fmt_int(GFX_DIMENSIONS_RECT_FROM_RIGHT_EDGE(71), 185, "%02d", timerSecs);
