0fd709462b0713ae47a37e125c6719a2ec09bc8f
AlexN <102762032+Al-ex-N@users.noreply.github.com>
2024-11-11 16:56:05 +0300

    Коммит 28.4

--- include/rus/consts/textures/intro_seg7_copyright.inc.c
+++ include/rus/consts/textures/intro_seg7_copyright.inc.c
@@ -1,7 +1,7 @@
-ALIGNED8 static const DIFF3_TYPE_TEXTURE intro_seg7_texture_rus_version[] = {
-#include "levels/intro/texture_rus_version.rgba16.inc.c"
+ALIGNED8 static const DIFF3_TYPE_TEXTURE intro_seg7_texture_rus_copyright_1[] = {
+#include "levels/intro/texture_rus_copyright_1.rgba16.inc.c"
 };
 
-ALIGNED8 static const DIFF3_TYPE_TEXTURE intro_seg7_texture_rus_copyright[] = {
-#include "levels/intro/texture_rus_copyright.rgba16.inc.c"
+ALIGNED8 static const DIFF3_TYPE_TEXTURE intro_seg7_texture_rus_copyright_2[] = {
+#include "levels/intro/texture_rus_copyright_2.rgba16.inc.c"
 };
--- include/rus/consts/vertexes/intro_seg7_copyright.inc.c
+++ include/rus/consts/vertexes/intro_seg7_copyright.inc.c
@@ -1,13 +1,13 @@
-static const Vtx intro_seg7_vertex_rus_version[] = {
-    {{{    43,     26,     -1}, 0, {     0,    512}, {0xff, 0xff, 0xff, 0xff}}},
-    {{{   171,     26,     -1}, 0, {  4096,    512}, {0xff, 0xff, 0xff, 0xff}}},
-    {{{   171,     42,     -1}, 0, {  4096,      0}, {0xff, 0xff, 0xff, 0xff}}},
-    {{{    43,     42,     -1}, 0, {     0,      0}, {0xff, 0xff, 0xff, 0xff}}},
+static const Vtx intro_seg7_vertex_rus_copyright_1[] = {
+    {{{    33,     27,     -1}, 0, {     0,    480}, {0xff, 0xff, 0xff, 0xff}}},
+    {{{   160,     27,     -1}, 0, {  4064,    480}, {0xff, 0xff, 0xff, 0xff}}},
+    {{{   160,     42,     -1}, 0, {  4064,      0}, {0xff, 0xff, 0xff, 0xff}}},
+    {{{    33,     42,     -1}, 0, {     0,      0}, {0xff, 0xff, 0xff, 0xff}}},
 };
 
-static const Vtx intro_seg7_vertex_rus_copyright[] = {
-    {{{   171,     26,     -1}, 0, {     0,    512}, {0xff, 0xff, 0xff, 0xff}}},
-    {{{   299,     26,     -1}, 0, {  4096,    512}, {0xff, 0xff, 0xff, 0xff}}},
-    {{{   299,     42,     -1}, 0, {  4096,      0}, {0xff, 0xff, 0xff, 0xff}}},
-    {{{   171,     42,     -1}, 0, {     0,      0}, {0xff, 0xff, 0xff, 0xff}}},
+static const Vtx intro_seg7_vertex_rus_copyright_2[] = {
+    {{{   160,     27,     -1}, 0, {     0,    480}, {0xff, 0xff, 0xff, 0xff}}},
+    {{{   287,     27,     -1}, 0, {  4064,    480}, {0xff, 0xff, 0xff, 0xff}}},
+    {{{   287,     42,     -1}, 0, {  4064,      0}, {0xff, 0xff, 0xff, 0xff}}},
+    {{{   160,     42,     -1}, 0, {     0,      0}, {0xff, 0xff, 0xff, 0xff}}},
 };
